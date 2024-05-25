34bee113a6f0af64bba1a5982d475b2e39ec00c7
Matheus Schiffer <43976069+Psychobellic@users.noreply.github.com>
2024-05-24 22:38:56 -0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,73 @@
+_G.love = require("love")
+
+function love.load()
+    math.randomseed(os.time())
+    love.graphics.setBackgroundColor(0.25, 0.25, 0.25)
+
+    _G.snake = {}
+    snake.length = 10
+    snake.x = 25
+    snake.y = 25
+    snake.direction = "E"
+    
+    _G.food = {}
+    food.x = math.random(1,799)
+    food.y = math.random(1,599)
+    food.eaten = false
+end
+
+function love.update()
+    if food.eaten then
+        snake.length = snake.length + 10
+        food.x = math.random(1,799)
+        food.y = math.random(1,599)
+        food.eaten = false
+    end
+
+    if snake.direction == "E" then
+        snake.x = snake.x + 3
+    elseif snake.direction == "W" then
+        snake.x = snake.x - 3
+    elseif snake.direction == "N" then
+        snake.y = snake.y - 3
+    elseif snake.direction == "S" then
+        snake.y = snake.y + 3
+    end
+
+    if love.keyboard.isDown("d") then
+        snake.direction = "E"
+    elseif love.keyboard.isDown("a") then
+        snake.direction = "W"
+    elseif love.keyboard.isDown("w") then
+        snake.direction = "N"
+    elseif love.keyboard.isDown("s") then
+        snake.direction = "S"
+    end
+
+    if snake.x <= 0 then
+        love.event.quit() 
+    elseif snake.x >= 800 then
+        love.event.quit() 
+    end
+    if snake.y <= 0 then
+        love.event.quit() 
+    elseif snake.y >= 600 then
+        love.event.quit() 
+    end
+end
+
+function love.draw()
+    if not food.eaten then
+        love.graphics.rectangle("fill", food.x, food.y, 10, 10)
+    end
+
+    if snake.direction == "E" then
+        love.graphics.rectangle("fill", snake.x, snake.y, snake.length, 10)
+    elseif snake.direction == "W" then   
+        love.graphics.rectangle("fill", snake.x, snake.y, snake.length, 10)
+    elseif snake.direction == "N" then
+        love.graphics.rectangle("fill", snake.x, snake.y, 10, snake.length)
+    elseif snake.direction == "S" then    
+        love.graphics.rectangle("fill", snake.x, snake.y, 10, snake.length)
+    end
+end
